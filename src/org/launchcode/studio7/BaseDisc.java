f17bb5062ceada069f60fe313b69b6d091a23ecc
KD Laptop <jill-sandwich@example.com>
2021-09-26 17:02:59 -0500

    completed studio 7 without bonus missions

---
+++ src/org/launchcode/studio7/BaseDisc.java
@@ -0,0 +1,60 @@
+package org.launchcode.studio7;
+
+import java.util.HashMap;
+
+public abstract class BaseDisc {
+    //class variable
+    private String discData;
+    private HashMap<String, String> discInfo;
+    private boolean isInserted;
+
+    private final int storageCapacityInMB;
+    private final int spinSpeedInRPM;
+
+    //constructor
+    public BaseDisc(int storageCapacity, int spinSpeed){
+        this.discData = "";
+        this.discInfo = new HashMap<String, String>();
+        this.isInserted = false;
+
+        this.storageCapacityInMB = storageCapacity;
+        this.spinSpeedInRPM = spinSpeed;
+
+        this.discInfo.put("Storage Capacity", String.valueOf(storageCapacity));
+        this.discInfo.put("Spin Speed", String.valueOf(spinSpeed));
+    }
+
+    public BaseDisc(int storageCapacity, int spinSpeed, String title){
+        this(storageCapacity, spinSpeed);
+        this.discInfo.put("Title", title);
+    }
+
+    //Getters and Setters
+    public boolean isInserted(){ return this.isInserted; }
+
+    public int getStorageCapacity(){ return this.storageCapacityInMB; }
+
+    public int getSpinSpeed(){ return this.spinSpeedInRPM; }
+
+    //methods
+    protected void writeData(String data){
+        this.discData += data;
+    }
+
+    public String readData(){
+        return this.discData;
+    }
+
+    public HashMap<String, String> getInfo(){
+        return this.discInfo;
+    }
+
+    public void insert(){
+        this.isInserted = true;
+    }
+
+
+    //rpm low, rpm high, data capacity, name
+
+
+}
--- src/org/launchcode/studio7/Main.java
+++ src/org/launchcode/studio7/Main.java
@@ -5,8 +5,27 @@ public class Main {
     public static void main(String[] args){
 
         // TODO: Declare and initialize a CD and a DVD object.
+         CD myCd = new CD();
+         DVD myDvd= new DVD("My Really Cool DVD");
 
         // TODO: Call each CD and DVD method to verify that they work as expected.
+        myCd.spinDisc();
+        myDvd.spinDisc();
+
+        myCd.writeData("More CD data to write");
+        System.out.println(myCd.readData());
+
+        myDvd.writeData("More DVD data to write");
+        System.out.println(myDvd.readData());
+
+
+        System.out.println(myCd.isFull());
+        System.out.println(myDvd.isFull());
+
+        myCd.insert();
+        myDvd.insert();
+        System.out.println(myCd.isInserted());
+        System.out.println(myDvd.isInserted());
 
     }
 }
